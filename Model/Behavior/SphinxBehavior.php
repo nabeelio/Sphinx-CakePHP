a40ff312a1ef165d5a30911cabf52c8fdf72a584
Nabeel Shahzad <nshahzad@gmail.com>
2016-07-19 12:39:22 -0500

    Tabs to spaces

--- Model/Behavior/SphinxBehavior.php
+++ Model/Behavior/SphinxBehavior.php
@@ -7,12 +7,10 @@
  * Collaborated and now maintained by Cake N Keyboard <https://github.com/CakeNKeyboard/Sphinx-CakePHP>
  * Updates @author CakeNKeyboard <https://github.com/CakeNKeyboard/>
  *
- * Derived from Nabeel Shahzad <https://github.com/nshahzad/Sphinx-CakePHP>
- *
- * @copyright 2008, Vilen Tambovtsev
- * @author  Vilen Tambovtsev
+ * Nabeel Shahzad <https://github.com/nabeelio/Sphinx-CakePHP>
  * @license      http://www.opensource.org/licenses/mit-license.php The MIT License
  */
+ 
 App::import('Vendor', 'gigablah/sphinxphp/src/Sphinx/SphinxClient');
 use Sphinx\SphinxClient;
 
@@ -23,7 +21,7 @@ class SphinxBehavior extends ModelBehavior {
      */
     public $runtime = array();
     public $_defaults = array('server' => 'localhost', 'port' => 9312);
-	public $total_results = null;
+    public $total_results = null;
 
     /**
      * Spinx client object
@@ -47,41 +45,41 @@ class SphinxBehavior extends ModelBehavior {
     public function beforeFind(Model $model, $query) {
 
         if (empty($query['sphinx']) || empty($query['search'])) {
-			return true;
-		}
+            return true;
+        }
 
         if ($model->findQueryType == 'count') {
 
             $model->recursive = -1;
             $query['limit'] = 1;
             $query['page'] = 1;
-			unset($query['conditions']);
-			unset($query['order']);
-			unset($query['offset']);
+            unset($query['conditions']);
+            unset($query['order']);
+            unset($query['offset']);
 
-			# We already ran the query, and stored the total number, prevent a duplicate query
-			# from running, just for the count - NS
-			if(!$this->total_results !== null) {
-				$this->total_results = !empty($this->total_results) ? $this->total_results : 0;
-				$query['fields'] = 'ABS(' . $this->total_results . ') AS count';
-				return $query;
-			}
+            # We already ran the query, and stored the total number, prevent a duplicate query
+            # from running, just for the count - NS
+            if(!$this->total_results !== null) {
+                $this->total_results = !empty($this->total_results) ? $this->total_results : 0;
+                $query['fields'] = 'ABS(' . $this->total_results . ') AS count';
+                return $query;
+            }
 
         } else if (empty($query['limit'])) {
             $query['limit'] = 9999999;
             $query['page'] = 1;
         }
 
-		$this->runtime[$model->alias]['sphinx'] = new SphinxClient();
-		$this->runtime[$model->alias]['sphinx']->setServer($this->settings[$model->alias]['server'], $this->settings[$model->alias]['port']);
+        $this->runtime[$model->alias]['sphinx'] = new SphinxClient();
+        $this->runtime[$model->alias]['sphinx']->setServer($this->settings[$model->alias]['server'], $this->settings[$model->alias]['port']);
 
         foreach ($query['sphinx'] as $key => $setting) {
 
             switch ($key) {
                 case 'filter':
-					if(empty($setting)) {
-						break;
-					}
+                    if(empty($setting)) {
+                        break;
+                    }
 
                     foreach ($setting as $arg) {
                         $arg[2] = empty($arg[2]) ? false : $arg[2];
@@ -112,7 +110,7 @@ class SphinxBehavior extends ModelBehavior {
         $this->runtime[$model->alias]['sphinx']->setLimits(($query['page'] - 1) * $query['limit'], $query['limit']);
         $indexes = !empty($query['sphinx']['index']) ? implode(',' , $query['sphinx']['index']) : '*';
         $result = $this->runtime[$model->alias]['sphinx']->query($query['search'], $indexes);
-		$this->total_results = !empty($result['total']) ? $result['total'] : 0;
+        $this->total_results = !empty($result['total']) ? $result['total'] : 0;
 
         if ($result === false) {
             trigger_error("Search query failed: " . $this->runtime[$model->alias]['sphinx']->GetLastError());
@@ -134,13 +132,13 @@ class SphinxBehavior extends ModelBehavior {
             $query['fields'] = 'ABS(' . $this->total_results . ') AS count';
         } else {
 
-			$model->sphinx_result = $result; # save to model for use - NS
+            $model->sphinx_result = $result; # save to model for use - NS
 
             if (isset($result['matches'])) {
-				$ids = array_keys($result['matches']);
-			} else {
-				$ids = array(0);
-			}
+                $ids = array_keys($result['matches']);
+            } else {
+                $ids = array(0);
+            }
 
             $query['conditions'] = array($model->alias . '.'.$model->primaryKey => $ids);
             $query['order'] = 'FIND_IN_SET('.$model->alias.'.'.$model->primaryKey.', \'' . implode(',', $ids) . '\')';
